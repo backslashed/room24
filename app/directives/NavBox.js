3806432c6602e5352b6987e8f77f183551a3d5cf
backslashed <gabriel@stripetree.co.uk>
2014-04-28 23:18:37 +0100

    Watered down spaghetti

--- app/directives/NavBox.js
+++ app/directives/NavBox.js
@@ -1,3 +1,8 @@
+'use strict';
+
+// Why oh why Gabriel must thy have this chunk of spaghetti? Because ngAnimate seems to misbehave when
+// applied on a repeater with stagger in Angular version 1.2 so we'll have to take compiling matters into
+// our own hands and animate using TweenMax.
 
 R24.NavBox = ['$interpolate', '$animate', '$location', function($interpolate, $animate, $location) {
 
@@ -5,41 +10,42 @@ R24.NavBox = ['$interpolate', '$animate', '$location', function($interpolate, $a
         var container    = angular.element(elem.children()[1]),
             header       = angular.element(elem.children()[0]).children(),
             itemTemplate = '<li><img src="{{thumbnail}}" alt="{{title}}" /><a href="#/category/{{category_id}}/{{id}}"><p>{{title}}<br />{{description}}<br /><span class="faded">{{agency}}</span></p></a><div class="overlay"></div></li>',
-            boxTemplate  = '<li><img src="{{thumbnail}}" alt="{{title}}" /><a><p>{{title}}</p></a><div class="overlay"></div></li>';
+            boxTemplate  = '<li><img src="{{thumbnail}}" alt="{{title}}" /><a><p>{{title}}</p></a><div class="overlay"></div></li>',
 
-        var pushNewItems = function(newItems) {
-            var template = $interpolate(newItems.boxClass == 'boxes' ? boxTemplate : itemTemplate);
+            popOldItems = function(items) {
+                TweenMax.staggerTo(items, 0.3, { opacity: 0, scale: 0.95 }, 0.5 / items.length);
+            },
 
-            container.html('');
+            pushNewItems = function(newItems) {
+                var template = $interpolate(newItems.boxClass == 'boxes' ? boxTemplate : itemTemplate);
 
-            elem.removeClass('boxes boxes-items').addClass(newItems.boxClass);
+                container.html('');
 
-            angular.forEach(newItems.items, function(item, key) {
-                var el = angular.element(template(item));
+                elem.removeClass('boxes boxes-items').addClass(newItems.boxClass);
 
-                if(newItems.boxClass === 'boxes') {
+                angular.forEach(newItems.items, function(item, key) {
+                    var el = angular.element(template(item));
+
                     el.bind('click', function() {
-                        scope.activeSlide = key + 1;
-                        scope.$apply();
-                    });
-                } else {
-                    el.bind('click', function(event) {
-                        $animate.addClass(header, 'getOut', function() {
-                            $location.path('/category/' + item.category_id + '/' + item.id);
+                        if(newItems.boxClass === 'boxes') {
+                            scope.activeSlide = key + 1;
                             scope.$apply();
-                        });
+                        } else {
+                            $animate.addClass(header, 'getOut', function () {
+                                $location.path('/category/' + item.category_id + '/' + item.id);
+                                scope.$apply();
+                            });
 
-                        TweenMax.staggerTo(container.children(), 0.3, { opacity: 0, scale: 0.95 }, 0.5 / container.children().length);
-
-                        event.preventDefault();
+                            popOldItems(container.children());
+                            event.preventDefault();
+                        }
                     });
-                }
 
-                container.append(el);
-            });
+                    container.append(el);
+                });
 
-            TweenMax.staggerTo(container.children(), 0.5, { opacity: 1, scale: 1 }, 0.07);
-        }
+                TweenMax.staggerTo(container.children(), 0.5, { opacity: 1, scale: 1 }, 0.07);
+            };
 
         scope.$watch('ngModel', function(newVal, oldVal) {
             if(angular.isDefined(newVal)) {
@@ -50,7 +56,7 @@ R24.NavBox = ['$interpolate', '$animate', '$location', function($interpolate, $a
                         $animate.removeClass(header, 'getOut');
                     });
 
-                    TweenMax.staggerTo(container.children(), 0.3, { opacity: 0, scale: 0.95 }, 0.5 / container.children().length);
+                    popOldItems(container.children());
                 } else {
                     header.html(newVal.body);
                     pushNewItems(newVal);
