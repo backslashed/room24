d673c94d6a15f14a33172fac6259a76a2330c56b
backslashed <gabriel@stripetree.co.uk>
2014-05-13 18:56:04 +0100

    Improved iOS animations

--- app/directives/NavBox.js
+++ app/directives/NavBox.js
@@ -1,24 +1,58 @@
 'use strict';
 
-// Why oh why Gabriel must thy have this chunk of spaghetti? Because ngAnimate seems to misbehave when
-// applied on a repeater with stagger in Angular version 1.2 so we'll have to take compiling matters into
-// our own hands and animate using TweenMax.
+R24.NavBox = ['$interpolate', '$animate', '$location', '$q', function($interpolate, $animate, $location, $q) {
 
-R24.NavBox = ['$interpolate', '$animate', '$location', function($interpolate, $animate, $location) {
+    var directiveConfig = {
+        restrict: 'E',
+        replace: true,
+        require: 'ngModel',
+        scope: {
+            'ngModel': '=',
+            'activeSlide': '='
+        },
+        template: '<nav ng-class="boxClass"><section class="body"><h1></h1></section><ul></ul></nav>',
+        link: constructor
+    }
 
-    var linkFn = function(scope, elem) {
+   function constructor(scope, elem) {
         var container    = angular.element(elem.children()[1]),
             header       = angular.element(elem.children()[0]).children(),
             itemTemplate = '<li><img src="{{thumbnail}}" alt="{{title}}" /><div class="hover-wrapper"><a href="#"><p>{{title}}<br />{{description}}<br /><span class="faded">{{agency}}</span></p></a></div><div class="overlay"></div></li>',
             boxTemplate  = '<li><img src="{{thumbnail}}" alt="{{title}}" /><div class="hover-wrapper"><a href="#"><p>{{title}}</p></a></div><div class="overlay"></div></li>',
 
-            popOldItems = function(items) {
-                TweenMax.staggerTo(items, 0.3, { opacity: 0, scale: 0.95 }, 0.35 / items.length);
+            hideSlideFor = function(newSlide) {
+                var all_     = $q.defer(),
+                    header_  = $q.defer(),
+                    boxes_  = $q.defer();
+
+
+                $animate.addClass(header, 'getOut', function() {
+                    $animate.removeClass(header, 'getIn', function() {
+                        header_.resolve();
+                    });
+                });
+
+                TweenMax.staggerTo(container.children(), 0.3, { opacity: 0, scale: 0.95 },
+                        newSlide.items.length > 12 ? 0.025 : 0.3 / newSlide.items.length, function() {
+                            boxes_.resolve();
+                        });
+
+                $q.all([ header_.promise, boxes_.promise ]).
+                    then(function() {
+                        all_.resolve(newSlide);
+                    });
+
+                return all_.promise;
             },
 
-            pushNewItems = function(newItems) {
-
+            showSlide = function(newItems) {
                 var template = $interpolate(newItems.boxClass == 'boxes' ? boxTemplate : itemTemplate);
+
+                header.html(newItems.body);
+
+                $animate.removeClass(header, 'getOut', function() {
+                    $animate.addClass(header, 'getIn');
+                });
 
                 container.html('');
                 elem.removeClass('boxes boxes-items').addClass(newItems.boxClass);
@@ -49,34 +83,26 @@ R24.NavBox = ['$interpolate', '$animate', '$location', function($interpolate, $a
                 TweenMax.staggerTo(container.children(), 0.35, { opacity: 1, scale: 1 }, 0.05);
             };
 
-        scope.$watch('ngModel', function(newVal, oldVal) {
-            if(angular.isDefined(newVal)) {
-                if(angular.isDefined(oldVal) && oldVal !== newVal && oldVal.items.length > 0) {
+        scope.$watch('ngModel', function(newSlide, oldSlide) {
+            if(angular.isDefined(newSlide)) {
+                if(angular.isDefined(oldSlide) && oldSlide !== newSlide && oldSlide.items.length > 0) {
+                    hideSlideFor(newSlide).then(showSlide);
+
+                    /*
                     $animate.addClass(header, 'getOut', function() {
                         pushNewItems(newVal);
                         header.html(newVal.body);
                         $animate.removeClass(header, 'getOut');
                     });
 
-                    popOldItems(container.children());
+                    popOldItems(container.children());*/
                 } else {
-                    header.html(newVal.body);
-                    pushNewItems(newVal);
+                    header.html(newSlide.body);
+                    showSlide(newSlide);
                 }
             }
         });
     }
 
-    return {
-        restrict: 'E',
-        replace: true,
-        require: 'ngModel',
-        scope: {
-            'ngModel': '=',
-            'activeSlide': '='
-        },
-        template: '<nav ng-class="boxClass"><section class="body"><h1></h1></section><ul></ul></nav>',
-
-        link: linkFn
-    }
+    return directiveConfig;
 }];
